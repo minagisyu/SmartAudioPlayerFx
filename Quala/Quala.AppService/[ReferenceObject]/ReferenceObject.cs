1f58d8e2d980df3f46d57903d1f3ca7401ad1951
MinagiSyu <minagisyu@gmail.com>
2016-09-25 00:21:04 +0900

    wip

---
+++ Quala/Quala.AppService/[ReferenceObject]/ReferenceObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quala
+{
+	/// <summary>
+	/// モデル向け多重参照されるオブジェクト
+	/// ReferenceObject.Managerクラスでインスタンスの生成、破棄を管理する
+	/// 内部に参照カウントを持ち、Dispose()呼び出し時に0で破棄処理が走る
+	/// </summary>
+	public partial class ReferenceObject
+	{
+		private ReferenceObject()
+		{
+
+		}
+
+	}
+}
